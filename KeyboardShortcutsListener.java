421ac5f81d18f8c92fba20243791e02048894712
Shreyas Nisal <shreyasnisal@gmail.com>
2019-12-13 22:48:48 +0530

    Keyboard shortcuts

--- KeyboardShortcutsListener.java
+++ KeyboardShortcutsListener.java
@@ -17,6 +17,26 @@ public class KeyboardShortcutsListener extends KeyAdapter {
 		if (e.getKeyCode() == KeyEvent.VK_S && (e.getModifiers() & KeyEvent.CTRL_MASK) != 0) {
 			FileOperations.saveFile(frame);
 		}
-
+		else if (e.getKeyCode() == KeyEvent.VK_N && (e.getModifiers() & KeyEvent.CTRL_MASK) != 0) {
+			//ctrl + n
+			FileOperations.newFile(frame);
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_O && (e.getModifiers() & KeyEvent.CTRL_MASK) != 0) {
+			//ctrl + o
+			FileOperations.openFile(frame);
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_B && (e.getModifiers() & KeyEvent.CTRL_MASK) != 0) {
+			//ctrl + b
+			PreferencesOperations.toggleBold(frame);
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_I && (e.getModifiers() & KeyEvent.CTRL_MASK) != 0) {
+			//ctrl + i
+			PreferencesOperations.toggleItalic(frame);
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_U && (e.getModifiers() & KeyEvent.CTRL_MASK) != 0) {
+			//ctrl + u
+			PreferencesOperations.toggleUnderline(frame);
+		}
+		
 	}
 }
